3c10fc81535b15909f1ccb303543f8661f354801
papamitra <papamitra@gmail.com>
2010-11-06 22:36:43 +0900

    MappedField.isをOptionに

--- src/main/scala/MappedInt.scala
+++ src/main/scala/MappedInt.scala
@@ -5,10 +5,4 @@ abstract class MappedInt[A <: Mapper[A]](val fieldOwner: A) extends MappedField[
   override def dbColumnTypeStr = "INTEGER"
 
   override def dbColumnType = classOf[java.lang.Integer]
-
-  override def isPrimaryKey = false
-}
-
-abstract class MappedIntIndex[A <: Mapper[A]](fieldOwner:A) extends MappedInt(fieldOwner) {
-  override def isPrimaryKey = true
 }
